b4c1291c535df34ed83fe00dd944ddf73a11e01a
Nigel Eke <nigeleke@gmail.com>
2023-08-23 16:26:53 +1000

    Initial run changes

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@
 mkdir -p target
 
 macro11 -rt11 -o target/susie.obj -l target/susie.lst src/susie.mac
+
+obj2bin.pl -rt11 --outfile=target/susie.bin target/susie.obj
+
+sudo cp target/susie.bin /opt/pidp11/systems/susie/
+
