70297dad194e69db19e02f082b3be11cec2e7de6
Nigel Eke <nigeleke@gmail.com>
2023-08-24 16:42:50 +1000

    Debugged console i/o

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ macro11 -rt11 -o target/susie.obj -l target/susie.lst src/susie.mac
 if [ -d "/opt/pidp11" ]
 then
     obj2bin.pl -rt11 --outfile=target/susie.bin target/susie.obj
+    sudo mkdir -p /opt/pidp11/systems/susie/
     sudo cp target/susie.bin /opt/pidp11/systems/susie/
+    sudo cp simh/boot.ini /opt/pidp11/systems/susie/
 fi
 
