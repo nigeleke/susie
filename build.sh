b54944c180d0aab46203ec30da8859bf5ad53ed6
Nigel Eke <nigeleke@gmail.com>
2023-08-24 23:48:11 +1000

    Add SUSIE:Load program

--- build.sh
+++ build.sh
@@ -6,11 +6,15 @@ macro11 -rt11 -o target/susie.obj -l target/susie.lst src/susie.mac
 macro11 -rt11 -o target/dkinit.obj -l target/dkinit.lst src/dkinit.mac
 macro11 -rt11 -o target/boot.obj -l target/boot.lst src/boot.mac
 
+macro11 -rt11 -o target/load.obj -l target/load.lst src/sysmac.sue src/load.sue
+
 if [ -d "/opt/pidp11" ]
 then
     obj2bin.pl -rt11 --outfile=target/susie.bin target/susie.obj
     obj2bin.pl -rt11 --outfile=target/dkinit.bin target/dkinit.obj
     obj2bin.pl -rt11 --outfile=target/boot.bin target/boot.obj
+
+    obj2bin.pl -rt11 --outfile=target/load.bin target/load.obj
 
     sudo cp simh/selections /opt/pidp11/systems/
 
@@ -25,4 +29,8 @@ then
     sudo mkdir -p /opt/pidp11/systems/boot/
     sudo cp target/boot.bin /opt/pidp11/systems/boot/boot.bin
     sudo cp simh/boot/boot.ini /opt/pidp11/systems/boot/
+
+    sudo mkdir -p /opt/pidp11/systems/load/
+    sudo cp target/load.bin /opt/pidp11/systems/load/load.bin
+    sudo cp simh/load/boot.ini /opt/pidp11/systems/load/
 fi
